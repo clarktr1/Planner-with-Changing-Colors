871dff2844853aee6b129481f8b3ca24c9f2518e
Trey Clark <itreyclark@gmail.com>
2023-01-06 21:14:12 -0600

    deletes comments from original file

--- Develop/script.js
+++ Develop/script.js
@@ -7,12 +7,7 @@ $(function () {
 
   var saveButton = document.querySelectorAll('button');
   var divEl = document.querySelectorAll('.time-block');
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   for (var i = 0; i < saveButton.length; i++){
     saveButton[i].addEventListener('click', saveInput);
   };
@@ -27,12 +22,6 @@ $(function () {
   };
 
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   for (var i = 0; i < divEl.length; i++) {
     var divBox = divEl[i];
     var now = dayjs().format('H');
@@ -47,18 +36,13 @@ $(function () {
     }   else if (now > blockTimeLast) {
           divBox.classList.remove('future', 'present');
           divBox.classList.add('past');
-    }   else if (now === blockTimeLast) {
+    }   else {
           divBox.classList.remove('future', 'past');
           divBox.classList.add('present');
       }
     };
 
 
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
     var textareaEl = document.querySelector('textarea');
 
@@ -71,14 +55,9 @@ $(function () {
     console.log(textArea);
 
     var value = localStorage.getItem(divBoxNumLast); //Grabs the value of the key number
-    console.log(value);
     textArea.innerHTML = value;
     };
 
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
   var currentDate = document.getElementById('currentDay');
   var timeHeader = document.querySelector('header');
 
